a7d9146a8d8d8727289a5320fc2d597fc9476934
Ankit Singh <ankitsingh2652003@gmail.com>
2025-06-14 12:04:48 +0530

    Day-4 Further challenges done

--- day4_challenges.py
+++ day4_challenges.py
@@ -18,3 +18,33 @@ finally:
 arr = np.array([5, 10, 15, 20, 25])
 print("Average=", np.mean(arr))
 print("Multiply=", arr * 2)
+
+a = np.array([1, 2, 3])
+b = np.array([[1, 2], [3, 4]])
+
+print("1D Array:", a)
+print("2D Array:\n", b)
+
+print(a[0])      
+print(b[1][1]) 
+
+print(a[1:])    # [2 3]
+print(b[:, 1])  # second column => [2 4]
+
+x = np.array([[1, 2], [3, 4]])
+y = np.array([[5, 6], [7, 8]])
+
+# Element-wise
+print(x + y)
+print(x * y)
+
+# Matrix multiplication
+print(x @ y)        # or np.dot(x, y)
+
+z = np.array([1, 2])
+print(x + z)
+# Output: [[2 4], [4 6]]
+
+print("Sum =", np.sum(x))
+print("Mean =", np.mean(x))
+print("Standard Deviation =", np.std(x))
